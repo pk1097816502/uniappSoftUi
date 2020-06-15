24e12600f7074337d20164e87d486f33e4c1b364
pk1097816502 <1097816502@qq.com>
2020-06-15 17:06:50 +0800

    新增softui基础实现

---
+++ components/Soft-UI/Basics.vue
@@ -0,0 +1,178 @@
+<template>
+	<view class="Basics" :style="style">
+		<slot></slot>
+	</view>
+</template>
+
+<script>
+	import {colorType} from "@/lib/regular.js";
+	
+	export default{
+		props:{
+			option:{
+				type:Object,
+				defalut:function(){
+					return {};
+				},
+				value:function(){
+					return {};
+				}
+			}
+		},
+		data(){
+			return{
+				shapeType:[
+					"default",
+					"press",
+					"button",
+					"disable"
+				]
+			}
+		},
+		computed:{
+			style(){
+				let tempStyle = {
+					"background-color" : this.color,
+					"width" : this.width+"rpx",
+					"height" : this.height+"rpx",
+					"border-radius" : this.radius+"rpx",
+					"box-shadow" : this.boxShadow
+				}
+				console.log("输出样式",tempStyle);
+				
+				return tempStyle;
+			},
+			color(){
+				if(this.option&&this.option.color){
+					return this.option.color;
+				}
+				return "";
+			},
+			width(){
+				if(this.option&&this.option.width){
+					return this.option.width;
+				}
+				return "";
+			},
+			height(){
+				if(this.option&&this.option.height){
+					return this.option.height;
+				}
+				return "";
+			},
+			size(){
+				return this.width>this.height?this.width:this.height
+			},
+			//圆角度数
+			radius(){
+				if(this.option&&this.option.radius){
+					return this.option.radius;
+				}
+				return parseInt(this.size/6);
+			},
+			//阴影宽度
+			distance(){
+				if(this.option&&this.option.distance){
+					return this.option.distance;
+				}
+				return parseInt(this.size/10);
+			},
+			//高光阴影倍数
+			intensity(){
+				if(this.option&&this.option.intensity){
+					return this.option.intensity;
+				}
+				return 0.15;
+			},
+			//阴影模糊程度
+			blur(){
+				if(this.option&&this.option.blur){
+					return this.option.blur;
+				}
+				return parseInt(this.size/5);
+			},
+			shape(){
+				if(this.option&&this.option.shape){
+					for(let type of this.shapeType){
+						if(this.option.shape==type){
+							return this.option.shape;
+						}
+					}
+				}
+				return "plane";
+			},
+			boxShadow(){
+				console.log("阴影颜色",this.shadowBoxShadow,this.highlightBoxShadow);
+				
+				let boxShadowStr = `${this.distance}rpx ${this.distance}rpx ${this.blur}rpx ${this.shadowBoxShadow},
+					-${this.distance}rpx -${this.distance}rpx ${this.blur}rpx ${this.highlightBoxShadow}`;
+				
+				if(this.shape=="disable"){
+					
+				}
+				return boxShadowStr;
+			},
+			//计算高光
+			highlightBoxShadow(){
+				let rgbNum = [];
+				rgbNum.push(...this.rgbNumList);
+				for(let i in rgbNum){
+					rgbNum[i] = parseInt(255-(1-this.intensity)*(255-rgbNum[i]));
+				}
+				let rgbStr = `rgb(${rgbNum[0]},${rgbNum[1]},${rgbNum[2]})`;
+				console.log("当前主题颜色值",this.color,rgbNum);
+				return rgbStr;
+			},
+			shadowBoxShadow(){
+				let rgbNum = [];
+				rgbNum.push(...this.rgbNumList);
+				for(let i in rgbNum){
+					// console.log("计算高光",(1-this.intensity)*num);
+					rgbNum[i] = parseInt((1-this.intensity)*rgbNum[i]);
+				}
+				let rgbStr = `rgb(${rgbNum[0]},${rgbNum[1]},${rgbNum[2]})`;
+				console.log("当前主题颜色值",this.color,rgbNum);
+				return rgbStr;
+			},
+			rgbNumList(){
+				let cType = colorType(this.color);
+				if(cType == "error"){
+					return "";
+				}else{
+					switch(cType){
+						case "color":
+							let tempColor = this.color.substr(1);
+							console.log("提取出来的颜色值",tempColor);
+							let tempRgbNumList = [0,0,0];
+							let digit = 1;
+							if(tempColor.length>3){
+								digit = 2;
+							}
+							for(let i in tempRgbNumList){
+								let tempNum = parseInt(tempColor.substr(i*digit,digit),16);
+								if(digit == 1){
+									tempNum = (tempNum+1)*16;
+								}
+								tempRgbNumList[i] = tempNum;
+							}
+							console.log("color提取出来的颜色",tempRgbNumList);
+							return tempRgbNumList;
+						break;
+					}
+				}
+				return "";
+			}
+		},
+		created() {
+			// console.log("color颜色是否合格",colorRegular("#ffffff"));
+			// console.log("rgb颜色是否合格",rgbRegular("rgb(255,255,255)"));
+			// console.log("rgba颜色是否合格",rgbaRegular("rgba(0,0,0,0.5)"));
+		},
+		mounted() {
+			// console.log("判断颜色类型",colorType("#ffffff"),this.style);
+		}
+	}
+</script>
+
+<style>
+</style>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,14 +1,16 @@
 <template>
-	<view class="content">
+	<view class="content":style="{'background-color':color}">
 		<Menu :menuList="menuList"></Menu>
+		<Basics :option="{color:color,width:'300',height:'300'}" class="basics"></Basics>
 	</view>
 </template>
 
 <script>
 	import Menu from "@/components/Menu.vue";
+	import Basics from "@/components/Soft-UI/Basics.vue";
 	
 	export default {
-		components:{Menu},
+		components:{Menu,Basics},
 		data() {
 			return {
 				menuList:[
@@ -16,7 +18,8 @@
 						label:"数字键盘",
 						url:"/pages/components/keyNumber"
 					}
-				]
+				],
+				color:"#111"
 			}
 		},
 		onLoad() {
@@ -28,6 +31,13 @@
 	}
 </script>
 
-<style lnag="scss" scoped>
-	
+<style lang="scss" scoped>
+	.content{
+		display: flex;
+		flex-direction: column;
+		
+		.basics{
+			margin: 50rpx;
+		}
+	}
 </style>
